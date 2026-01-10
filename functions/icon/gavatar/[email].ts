5c750234b41f4c84da24e3a648f126b1d573c56e
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2026-01-10 15:42:51 +0800

    add default size for gavatar requests

--- functions/icon/gavatar/[email].ts
+++ functions/icon/gavatar/[email].ts
@@ -3,7 +3,7 @@ declare module "js-sha256";
 
 export const onRequest: PagesFunction = async (context) => {
     const { request, params } = context;
-    const size = new URL(request.url).searchParams.get("s");
+    const size = new URL(request.url).searchParams.get("s") || "80";
     const email = params.email as string;
     const SHA256Email = sha256(email);
     return await fetch(`https://gravatar.com/avatar/${SHA256Email}?s=${size}`);
--- functions/icon/hashed-gavatar/[hash].ts
+++ functions/icon/hashed-gavatar/[hash].ts
@@ -1,6 +1,6 @@
 export const onRequest: PagesFunction = async (context) => {
     const { request, params } = context;
-    const size = new URL(request.url).searchParams.get("s");
+    const size = new URL(request.url).searchParams.get("s") || "80";
     const SHA256Email = params.hash as string;
     return await fetch(`https://gravatar.com/avatar/${SHA256Email}?s=${size}`);
 }
